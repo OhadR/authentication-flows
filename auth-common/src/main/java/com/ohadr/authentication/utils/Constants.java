27286efa561bd678e30465c66c552c85765d40f1
OhadR <ohadr.developer@gmail.com>
2013-11-21 12:50:04 +0200

    add consts and encrypt utils

---
+++ auth-common/src/main/java/com/ohadr/authentication/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.ohadr.authentication.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Constants
+{
+
+	public static final String HASH_PARAM_NAME = "enc";
+	public static final String REDIRECT_URI_PARAM_NAME = "redirect_uri";
+
+}
---
+++ auth-common/src/main/java/com/ohadr/authentication/utils/OAuthUtils.java
@@ -0,0 +1,36 @@
+package com.ohadr.authentication.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.apache.log4j.Logger;
+
+public abstract class OAuthUtils
+{
+    private static final Logger log = Logger.getLogger(OAuthUtils.class);
+
+	public static final String OAUTH_WEB_APP_NAME = "oauth-srv";
+	public static final String ENCRYPTED_USERNAME_PARAM_NAME = "e";
+
+	/**
+	 * the SDK does not know how to encode params in the URL (only JSONs in the body...)
+	 * so this util does the work
+	 * @param param
+	 * @return
+	 */
+	public static String encodeParam(String param)
+	{
+		String encodedParam = null;
+		try
+		{
+			encodedParam = URLEncoder.encode( param, "utf8" );
+		}
+		catch (UnsupportedEncodingException e)
+		{
+			String errorText = e.getMessage();
+			log.error(errorText);
+		}
+		return encodedParam;
+	}	
+
+}
