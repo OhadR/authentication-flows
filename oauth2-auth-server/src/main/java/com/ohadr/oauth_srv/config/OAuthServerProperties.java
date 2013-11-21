f2946f9fee2e22695e31d88dd2350ef3415cb018
OhadR <ohadr.developer@gmail.com>
2013-11-21 15:09:52 +0200

    add oauth-srv-props

---
+++ oauth2-auth-server/src/main/java/com/ohadr/oauth_srv/config/OAuthServerProperties.java
@@ -0,0 +1,29 @@
+package com.ohadr.oauth_srv.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OAuthServerProperties
+{
+	/**
+	 * the time in seconds of how long the links that the oauth-srv generates are valid. relevant for the "forgot password" flow, 
+	 * where the oauth-srv makes the expiration check. (for other flows, createUser and activateAccount - checks are in the webapp)
+	 */
+	@Value("${com.watchdox.oauth2.linksExpirationMinutes}")
+	private int linksExpirationMinutes;
+
+
+	
+	
+	public int getLinksExpirationMinutes()
+	{
+		return linksExpirationMinutes;
+	}
+
+	public void setLinksExpirationMinutes(int linksExpirationMinutes)
+	{
+		this.linksExpirationMinutes = linksExpirationMinutes;
+	}
+
+}
