df023a8e1301ae0c37978aa68e82866ef9df63f6
OhadR <ohadr.developer@gmail.com>
2013-11-29 00:53:54 +0200

    1. fix mailSender looger 2. rename link-expiration property

--- authentication-flows/src/main/java/com/ohadr/auth_flows/mocks/MailSenderMock.java
+++ authentication-flows/src/main/java/com/ohadr/auth_flows/mocks/MailSenderMock.java
@@ -3,14 +3,13 @@ package com.ohadr.auth_flows.mocks;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
-import com.ohadr.auth_flows.core.AuthenticationFlowsProcessorImpl;
 import com.ohadr.auth_flows.interfaces.MailSender;
 
 
 @Service
 public class MailSenderMock implements MailSender
 {
-	private static Logger log = Logger.getLogger(AuthenticationFlowsProcessorImpl.class);
+	private static Logger log = Logger.getLogger(MailSenderMock.class);
 
 	@Override
 	public void sendMail(String adressee, String subject, String mailBody) 
