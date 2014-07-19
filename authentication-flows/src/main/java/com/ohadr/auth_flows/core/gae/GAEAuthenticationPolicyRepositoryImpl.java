d86f3da1a3befd1e221e4df0a35d8c8bd06dee13
OhadR <ohadr.developer@gmail.com>
2014-07-19 12:09:53 +0300

    AuthenticationPolicyRepository - support GAE #44

---
+++ authentication-flows/src/main/java/com/ohadr/auth_flows/core/gae/GAEAuthenticationPolicyRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.ohadr.auth_flows.core.gae;
+
+import com.ohadr.auth_flows.interfaces.AuthenticationPolicyRepository;
+import com.ohadr.auth_flows.types.AuthenticationPolicy;
+
+public class GAEAuthenticationPolicyRepositoryImpl implements
+		AuthenticationPolicyRepository
+{
+
+	@Override
+	public AuthenticationPolicy getDefaultAuthenticationPolicy()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AuthenticationPolicy getAuthenticationPolicy(int settingsId)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
