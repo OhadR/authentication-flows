2572002e865491c87ebc8c62e6e40f8ae3396d95
OhadR <ohadr.developer@gmail.com>
2014-10-29 20:24:55 +0200

    rollback #63 - not needed !

--- authentication-flows/src/main/java/com/ohadr/auth_flows/core/AuthenticationSuccessHandler.java
+++ authentication-flows/src/main/java/com/ohadr/auth_flows/core/AuthenticationSuccessHandler.java
@@ -13,8 +13,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Service;
 
-import com.ohadr.auth_flows.endpoints.CreateAccountEndpoint;
-import com.ohadr.auth_flows.endpoints.LoginEndpoint;
 import com.ohadr.auth_flows.interfaces.AuthenticationFlowsProcessor;
 import com.ohadr.auth_flows.types.FlowsConstatns;
 import com.ohadr.crypto.service.CryptoService;
@@ -28,9 +26,6 @@ public class AuthenticationSuccessHandler extends
 
 	@Autowired
 	private CryptoService cryptoService;
-
-	@Autowired(required=false)
-	private LoginEndpoint loginEndpoint = new LoginEndpoint();
 
 	private static Logger log = Logger.getLogger(AuthenticationSuccessHandler.class);
 
@@ -64,9 +59,6 @@ public class AuthenticationSuccessHandler extends
 		// changeSessionTimeout(request);
 		/////////////////////////////////////////
 
-		//let applications do a custom-behavior upon login (e.g. count logins etc.):
-		loginEndpoint.postLogin( username );
-		
     	super.onAuthenticationSuccess(request, response, authentication);
     }
     
