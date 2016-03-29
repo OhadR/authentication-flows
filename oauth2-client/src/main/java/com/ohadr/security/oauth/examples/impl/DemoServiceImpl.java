74826f0270f4e0c964abf1dc505fee3346c12dee
OhadR <ohadr.developer@gmail.com>
2016-03-29 18:35:34 +0300

    [solve] java.security.cert.CertificateException: No name matching localhost found
    refer to this:
    http://www.mkyong.com/webservices/jax-ws/java-security-cert-certificateexception-no-name-matching-localhost-found/

--- oauth2-client/src/main/java/com/ohadr/security/oauth/examples/impl/DemoServiceImpl.java
+++ oauth2-client/src/main/java/com/ohadr/security/oauth/examples/impl/DemoServiceImpl.java
@@ -10,6 +10,20 @@ import java.net.URI;
 
 public class DemoServiceImpl implements DemoService
 {
+	static {
+	    //for localhost testing only
+	    javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
+	    new javax.net.ssl.HostnameVerifier(){
+
+	        public boolean verify(String hostname,
+	                javax.net.ssl.SSLSession sslSession) {
+	            if (hostname.equals("localhost")) {
+	                return true;
+	            }
+	            return false;
+	        }
+	    });
+	}
 
 	@Autowired
 	private RestOperations butkeDemoRestTemplate;
